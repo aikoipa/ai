76b070e4324b8c22f3ca950cebb83541471e001c
Juwon Jeong <2jw0718@gmail.com>
2020-05-15 11:30:54 +0900

    Updates

---
+++ precache-manifest.d82e9fc1efb7bb9a598ad01a46c5cd1a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf25b447bbfd79bb5eb5fc0997292891",
+    "url": "/ai/index.html"
+  },
+  {
+    "revision": "ccad4ca1ba56b6fc1311",
+    "url": "/ai/static/css/main.14b7b3d1.chunk.css"
+  },
+  {
+    "revision": "5498be9892fdcaadf348",
+    "url": "/ai/static/js/2.deef6ea6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ai/static/js/2.deef6ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccad4ca1ba56b6fc1311",
+    "url": "/ai/static/js/main.31013fba.chunk.js"
+  },
+  {
+    "revision": "aeadb1ba0fa6f66d59b6",
+    "url": "/ai/static/js/runtime-main.62f6eb70.js"
+  },
+  {
+    "revision": "231ef82653d90aa19929e7dcd84c55c7",
+    "url": "/ai/static/media/noto-sans-kr-v12-korean_latin-regular.231ef826.woff"
+  },
+  {
+    "revision": "44d80edbb024d08c79042218e7277f68",
+    "url": "/ai/static/media/noto-sans-kr-v12-korean_latin-regular.44d80edb.svg"
+  },
+  {
+    "revision": "56403fb85d9df85df3426a5d2df5aeeb",
+    "url": "/ai/static/media/noto-sans-kr-v12-korean_latin-regular.56403fb8.eot"
+  },
+  {
+    "revision": "7b91af55b8d0b4df6adc4c5d6810037a",
+    "url": "/ai/static/media/noto-sans-kr-v12-korean_latin-regular.7b91af55.woff2"
+  }
+]);

629ebbccb006e41416b0909cf71afd4a13c82d00
Juwon Jeong <2jw0718@gmail.com>
2021-05-11 04:11:22 +0900

    update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
       <div className="section2">
         {/* <div className="round-top" /> */}
-         <img src="/images/poster_20210501.jpg" alt="poster" className="poster"/>
+         <img src="/images/poster_20210511.jpg" alt="poster" className="poster"/>
         {/* <div className="round-bottom" /> */}
       </div>
       <div className="section7">
